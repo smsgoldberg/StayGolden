99933e036fa15ac286f00d6968e3a9f6cea083ea
Sarah <sarahspagnolagoldberg@gmail.com>
2023-08-07 14:49:17 -0400

    adjust array and object placement

--- js/main.js
+++ js/main.js
@@ -1,8 +1,46 @@
+const imageArray = [];
+
+/*---objects---*/
+
+class dog {
+  constructor(number, backgroundColor) {
+      this.number = number;
+      this.backgroundColor = backgroundColor;
+  }
+}
+
+const swimmingDog = new dog(1, 'blue');
+imageArray.push(swimmingDog);
+
+const smilingDog = new dog(2, 'green');
+imageArray.push(smilingDog);
+
+const puppyDog = new dog(3, 'yellow');
+imageArray.push(puppyDog);
+
+const olderDog = new dog(4, 'red');
+imageArray.push(olderDog);
+
+const sleepingDog = new dog(5, 'purple');
+imageArray.push(sleepingDog);
+
+const jumpingDog = new dog(6, 'white'); 
+imageArray.push(jumpingDog);
+
+const ballDog = new dog(7, 'black');
+imageArray.push(ballDog);
+
+const runningDog = new dog(8, 'orange');
+imageArray.push(runningDog);
+
+
+
 /*-----constants -----*/
 //we will keep all our images in an image array - and randomize their positioning everytime the game is initialized
-const imageArray = [];
+//const imageArray = [];
 const matchArray = imageArray.map((x) => x * 1);
 const mergedDogArray = imageArray.concat(matchArray);
+console.log(mergedDogArray);
 
 /*-----state variables ----*/
 let board;
@@ -12,7 +50,7 @@ let endCondition;
 /*---cached elements---*/
  let scoreboard = document.querySelector('h2');
  let playAgainButton = document.querySelector('button');
- let tiles = document.querySelector('.circles');
+ let tiles = [...document.querySelectorAll('.circles')];
 
 /* --- event listeners ---  */
 //enables player to reset the board for a new game
@@ -61,36 +99,37 @@ function populateBoard() {
 
 
 
-/*---objects---*/
+// / /*---objects---*/
 
-class dog {
-    constructor(number, backgroundColor) {
-        this.number = number;
-        this.backgroundColor = backgroundColor;
-    }
-}
+// class dog {
+//     constructor(number, backgroundColor) {
+//         this.number = number;
+//         this.backgroundColor = backgroundColor;
+//     }
+// }
 
-const swimmingDog = new dog(1, 'blue');
-imageArray.push(swimmingDog);
+// const swimmingDog = new dog(1, 'blue');
+// imageArray.push(swimmingDog);
 
-const smilingDog = new dog(2, 'green');
-imageArray.push(smilingDog);
+// const smilingDog = new dog(2, 'green');
+// imageArray.push(smilingDog);
 
-const puppyDog = new dog(3, 'yellow');
-imageArray.push(puppyDog);
+// const puppyDog = new dog(3, 'yellow');
+// imageArray.push(puppyDog);
 
-const olderDog = new dog(4, 'red');
-imageArray.push(olderDog);
+// const olderDog = new dog(4, 'red');
+// imageArray.push(olderDog);
 
-const sleepingDog = new dog(5, 'purple');
-imageArray.push(sleepingDog);
+// const sleepingDog = new dog(5, 'purple');
+// imageArray.push(sleepingDog);
 
-const jumpingDog = new dog(6, 'white'); 
-imageArray.push(jumpingDog);
+// const jumpingDog = new dog(6, 'white'); 
+// imageArray.push(jumpingDog);
 
-const ballDog = new dog(7, 'black');
-imageArray.push(ballDog);
+// const ballDog = new dog(7, 'black');
+// imageArray.push(ballDog);
 
-const runningDog = new dog(8, 'orange');
-imageArray.push(runningDog);
+// const runningDog = new dog(8, 'orange');
+// imageArray.push(runningDog);
+
 
